15b9121f92fb8cd9b04e8516d8c7c9d41863c052
valeriomatranga <matrangav@gmail.com>
2021-05-11 23:12:54 +0200

    snak2

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,62 @@ falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+let squadre = [
+    {
+        'nome': 'palermo',
+        'punti_fatti': 0,
+        'falli_subiti': 0
+    },
+    {
+        'nome': 'cagliari',
+        'punti_fatti': 0,
+        'falli_subiti': 0
+    },
+    {
+        'nome': 'milan',
+        'punti_fatti': 0,
+        'falli_subiti': 0
+    },
+    {
+        'nome': 'fiorentina',
+        'punti_fatti': 0,
+        'falli_subiti': 0
+    },
+];
+
+let [{punti_fatti}] = squadre;
+let [{falli_subiti}] = squadre;
+let [{nome}] = squadre;
+
+function mathRandom (lista,numero1,numero2){
+    
+    for(var i = 0; i < lista.length; i++){
+        
+        
+        if(numero1 === 0){
+            punti_fatti = (Math.floor(Math.random()*100));
+            //console.log(punti_fatti);
+        }
+        
+        if( numero2 == 0){
+            falli_subiti = (Math.floor(Math.random()*100));
+            //console.log(falli_subiti);
+        }
+
+        punteggio.push({nome,falli_subiti});
+
+    }
+}
+
+
+let punteggio = [];
+
+
+mathRandom(squadre,punti_fatti,falli_subiti);
+
+
+console.log(punteggio);
+
+
+
